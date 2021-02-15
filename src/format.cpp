b727eeccefbdb7e601211a7e761ee55d6a57f6ae
Carlos G. Gavidia <cgavidiac@gmail.com>
2021-02-15 22:03:22 +0000

    Reusing the methods from LinuxParser already implemented.

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,40 @@
 #include <string>
+#include <iomanip>
 
 #include "format.h"
 
 using std::string;
+using std::stringstream;
 
-// TODO: Complete this helper function
+
+string FormatNumber(long number) {
+    stringstream string_stream;
+    string_stream << std::setw(2) << std::setfill('0') << number;
+
+    return string_stream.str();
+}
+
+// Helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    long total_seconds = seconds;
+    long total_hours = 0;
+    long total_minutes = 0;
+
+    const int seconds_in_minute = 60;
+    const int seconds_in_hour = 60 * seconds_in_minute;
+
+    if (total_seconds >= seconds_in_hour) {
+        total_hours = total_seconds / seconds_in_hour;
+        total_seconds = total_seconds % seconds_in_hour;
+    }
+
+    if (total_seconds >= seconds_in_minute) {
+        total_minutes = total_seconds / seconds_in_minute;
+        total_seconds = total_seconds % seconds_in_minute;
+    } 
+
+
+    return FormatNumber(total_hours) + FormatNumber(total_minutes) + FormatNumber(total_seconds); 
+}
